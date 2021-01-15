18a888db92203f91cf9d2c50074cecda40969197
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2021-01-15 18:22:51 +0100

    Handle api error in PostPage

--- src/components/PostPage.js
+++ src/components/PostPage.js
@@ -6,50 +6,62 @@ import Comments from './Comments'
 import { formatDate } from '../utils/helpers'
 import ThemeContext from '../contexts/ThemeContext'
 import Loading from './Loading'
+import ErrorMessage from './ErrorMessage'
 
 class PostPage extends Component {
   state = {
+    error: null,
     post: null,
   }
 
   componentDidMount() {
     const postId = new URLSearchParams(this.props.location.search).get('id')
     console.log('PostPage componentDidMount postId', postId)
-    fetchItem(postId).then((post) => {
-      console.log('post', postId, post)
-      this.setState({ post })
-    })
+    fetchItem(postId)
+      .then((post) => {
+        console.log('post', postId, post)
+        this.setState({ post })
+      })
+      .catch((error) => {
+        console.error('PostPage', error)
+        this.setState({ error })
+      })
   }
 
   render() {
-    const { post } = this.state
+    const { error, post } = this.state
     const theme = this.context
+
+    if (error !== null) {
+      return <ErrorMessage />
+    }
 
     if (post === null) {
       return <Loading text="Fetching Post" />
-    } else {
-      return (
-        <>
-          <h1 className="header">
-            <a className="link" href={post.url}>
-              {post.title}
-            </a>
-          </h1>
-          <div className={`meta-info-${theme}`}>
-            <span>
-              by <Link to={`/user?id=${post.by}`}>{post.by}</Link>
-            </span>
-            <span>on {formatDate(post.time)}</span>
-            <span>
-              with <Link to={`/post?id=${post.id}`}>{post.descendants}</Link>{' '}
-              comments
-            </span>
-          </div>
-          <p></p>
-          {post.descendants !== 0 && <Comments commentIds={post.kids} />}
-        </>
-      )
     }
+
+    return (
+      <>
+        <h1 className="header">
+          <a className="link" href={post.url}>
+            {post.title}
+          </a>
+        </h1>
+        <div className={`meta-info-${theme}`}>
+          <span>
+            by <Link to={`/user?id=${post.by}`}>{post.by}</Link>
+          </span>
+          <span>on {formatDate(post.time)}</span>
+          <span>
+            with <Link to={`/post?id=${post.id}`}>{post.descendants}</Link>{' '}
+            comments
+          </span>
+        </div>
+        {/* This empty <p></p> appears on the solution :thinking: */}
+        <p></p>
+        {post.descendants !== 0 && <Comments commentIds={post.kids} />}
+      </>
+    )
   }
 }
 
