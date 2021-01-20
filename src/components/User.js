908db76cefa50b34ee6b7496f3684a50cb8d1411
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2021-01-20 20:52:36 +0100

    Convert User class component to function with hooks

--- src/components/User.js
+++ src/components/User.js
@@ -1,4 +1,4 @@
-import { Component } from 'react'
+import { useEffect, useState, useContext } from 'react'
 import PropTypes from 'prop-types'
 import { fetchUser } from '../utils/api'
 import UserPosts from './UserPosts'
@@ -7,58 +7,54 @@ import ThemeContext from '../contexts/ThemeContext'
 import Loading from './Loading'
 import ErrorMessage from './ErrorMessage'
 
-class User extends Component {
-  state = {
+function User({ location }) {
+  const userId = new URLSearchParams(location.search).get('id')
+  const [state, setState] = useState({
     error: null,
     user: null,
-  }
+  })
 
-  componentDidMount() {
-    const userId = new URLSearchParams(this.props.location.search).get('id')
+  useEffect(() => {
     console.log('User componentDidMount userId', userId)
     fetchUser(userId)
       .then((user) => {
         console.log('user', userId, user)
-        this.setState({ user })
+        setState({ user, error: null })
       })
       .catch((error) => {
         console.error('User', error)
-        this.setState({ error })
+        setState({ user: null, error })
       })
+  }, [userId])
+
+  const { error, user } = state
+  const theme = useContext(ThemeContext)
+
+  if (error !== null) {
+    return <ErrorMessage />
   }
 
-  render() {
-    const { error, user } = this.state
-    const theme = this.context
-
-    if (error !== null) {
-      return <ErrorMessage />
-    }
-
-    if (user === null) {
-      return <Loading text="Fetching User" />
-    }
-
-    return (
-      <>
-        <h1 className="header">{user.id}</h1>
-        <div className={`meta-info-${theme}`}>
-          <span>
-            joined <b>{formatDate(user.created)}</b>
-          </span>
-          <span>
-            has <b>{user.karma.toLocaleString()}</b> karma
-          </span>
-        </div>
-        <p dangerouslySetInnerHTML={{ __html: user.about }} />
-        <h2>Posts</h2>
-        <UserPosts postIds={user.submitted} />
-      </>
-    )
+  if (user === null) {
+    return <Loading text="Fetching User" />
   }
+
+  return (
+    <>
+      <h1 className="header">{user.id}</h1>
+      <div className={`meta-info-${theme}`}>
+        <span>
+          joined <b>{formatDate(user.created)}</b>
+        </span>
+        <span>
+          has <b>{user.karma.toLocaleString()}</b> karma
+        </span>
+      </div>
+      <p dangerouslySetInnerHTML={{ __html: user.about }} />
+      <h2>Posts</h2>
+      <UserPosts postIds={user.submitted} />
+    </>
+  )
 }
-
-User.contextType = ThemeContext
 
 User.propTypes = {}
 
